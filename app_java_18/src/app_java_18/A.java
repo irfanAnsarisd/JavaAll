3b4e3177412c88e2aca516f409bcef3730c659ee
Irfan_Ansari <109786356+irfanAnsarisd@users.noreply.github.com>
2024-09-12 11:03:40 +0530

    exception handling

---
+++ app_java_18/src/app_java_18/A.java
@@ -0,0 +1,18 @@
+package app_java_18;
+
+public class A {
+	int a = 10;
+	public static void main(String[] args) {
+		
+		// wrapper class
+		try {
+			String x = "dkkfkl3003";
+			int y = Integer.parseInt(x);
+			System.out.println(y);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("welcome");
+}
+}
