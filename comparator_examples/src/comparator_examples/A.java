bc0850d3c412a9511940b470a7d9b0e0957ddfcb
Irfan_Ansari <109786356+irfanAnsarisd@users.noreply.github.com>
2024-09-12 11:07:18 +0530

    comparator

---
+++ comparator_examples/src/comparator_examples/A.java
@@ -0,0 +1,23 @@
+//package comparator_examples;
+//
+//import java.util.Comparator;
+//
+//public class A implements Comparator<Students>{
+//  public static void main(String[] args) {
+////	String x = "abc";
+////	String y = "xyz";
+////	System.out.println(y.compareTo(x));
+//	  A a1 = new A();
+//	
+//	  int val = a1.compare(new Students(100,"xyz"), new Students(200,"abc"));
+//	  System.out.println(val);
+//	  
+//}
+//
+//@Override
+//public int compare(Students o1, Students o2) {
+//	
+//	return o1.name.compareTo(o2.name);
+//}
+//
+//}
---
+++ comparator_examples/src/comparator_examples/Students.java
@@ -0,0 +1,11 @@
+package comparator_examples;
+
+public class Students {
+	int id;
+	String name;
+	
+	Students(int id, String name){
+		this.id = id;
+		this.name = name;
+	}
+}
